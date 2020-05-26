082b7ae3b5d003d1c5eb5a350f14b85f2ec833dd
Shubham <s.shrinath@splendornet.com>
2020-05-26 22:51:44 +0530

    Added : Multiplication and Addition functionality

--- airthmatic.sh
+++ airthmatic.sh
@@ -5,4 +5,6 @@ read -p "Enter the value a: " a
 read -p "Enter the value b: " b
 read -p "Enter the value c: " c
 
-result=$((a+$((b*c))))
+result_first=$((a+$((b*c))))
+
+result_second=$(($((b*c))+a))
